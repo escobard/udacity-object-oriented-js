7fd200492a7e2442d973906916ccce2634c06484
Daniel Escobar <escobard90@gmail.com>
2016-10-03 21:41:45 -0600

    Added functional shared patterns to save space and memory by only writing construction function supporting functions in one place

--- library.js
+++ library.js
@@ -2,14 +2,20 @@
 // decorator augments the object its going to agument / change
 var car = function (loc){
 var obj  = {loc: loc};
-obj.move = move;
+// extends the object by adding functions that extend its functionality via a single callback
+extend(obj, methods);
 return obj;
 };
 
 // moved the move function out of the car constructor function to not create a function per instance of the car function, saving memory
-var move = function (){
+// moved move and other custom functions into the methods variable to only write the functions attached to each object iteration once 
+var methods = {
+	move : function (){
 	removeCarFromScreen(this.oc);
 	addDustSwirlToScreen(this.loc);
 	this.loc++;
 	addCarToScreen(this.loc);
+},
+on : function (){},
+off : function (){}
 };
