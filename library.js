280b00d794cde3b450cc62c8767671c06fc4c061
Daniel Escobar <escobard90@gmail.com>
2016-10-03 21:26:32 -0600

    Added the this parameter and separated the move function to avoid using up memory

--- library.js
+++ library.js
@@ -1,10 +1,15 @@
+// using class - builds the object that is going to augment / change
+// decorator augments the object its going to agument / change
 var car = function (loc){
 var obj  = {loc: loc};
-obj.move = function(){
-	removeCarFromScreen(obj.loc);
-	addDustSwirlToScreen(obj.loc);
-	obj.loc++;
-	addCarToScreen(obj.loc);
-};
+obj.move = move;
 return obj;
+};
+
+// moved the move function out of the car constructor function to not create a function per instance of the car function, saving memory
+var move = function (){
+	removeCarFromScreen(this.oc);
+	addDustSwirlToScreen(this.loc);
+	this.loc++;
+	addCarToScreen(this.loc);
 };
