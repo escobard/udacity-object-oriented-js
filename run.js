fd6e290c431d6ce0e2f4fd35fe444d8d9e3f5dce
Daniel Escobar <escobard90@gmail.com>
2016-10-17 20:52:19 -0600

    Added the answer to the quiz on prototype.constructor properties

--- run.js
+++ run.js
@@ -2,3 +2,11 @@ var amy = Car(1);
 amy.move();
 var ben = Car(9);
 ban.move();
+
+// the prototype.constructor property essentially mirrors the value of its parent object
+// for example refer to below:
+console.log(Car.prototype.constructor);
+// the above callback would duplicate the EXACT calues of the Car variable.
+// another example is:
+console.log(amy.constructor);
+// this calls the Car variable, since Car is the constructor of the Amy variable
