cc9e38b19b9b6a27cb56446db4da32ed1b496042
Daniel Escobar <escobard90@gmail.com>
2016-10-21 21:50:06 -0600

    re-wrote code for pseuodo classical unit

--- library.js
+++ library.js
@@ -1,66 +1,20 @@
-// using class - builds the object that is going to augment / change
-// decorator augments the object its going to agument / change
-var Car = function (loc){
-
-// added the Object.create method instead of a literal object to allow prototyping
-// replaced Car.methods property to Car.prototype in order to call the object's methods via the prototype callback.
-// removed the property of the Car object that initiate a prototype, since the new constructor was added to each object instance.
-//	var obj  = Object.create(Car.prototype);
-
-// add the property loc to the obj object
-// removed since the new constructor has been added to each object instance
-//	obj.loc = loc;
-
-	this.loc = loc;
-
-// extends the object by adding functions that extend its functionality via a single callback.
-// changed to instead call as a method of the car object, to avoid calling a global variable.
-
-// got rid of the extend function, as the object Car.methods is already being called to by the object.create callback.
-// extend(obj, Car.methods);
-// removed the return property of the Car object since the new constructor is being used for each instance of the Car object.
-//	return obj;
-
-// the new constructor on any instance of an object automatically attaches the following lines of code to the called object; the following two lines are not necessary
-// but were added for reference purposes
-	this = Object.create(Car.prototype);
-	return this;
+// re-written for pseudoclassical subclasses unit
+var Car = function() {
+	var obj = {loc: loc};
+	obj.move = function (){
+		obj.loc++
+	}
+	return obj;
 };
 
-// moved the move function out of the car constructor function to not create a function per instance of the car function, saving memory
-// moved move and other custom functions into the methods variable to only write the functions attached to each object iteration once 
-// changed to accept the prototype property of the Car object.
-Car.prototype.move = function (){
-	this.loc++;
+var Van = function (loc){
+	var obj = Car(loc);
+	obj.grab = function {}
+	return obj;
 };
-// very important to remember that any .prototype designation is basically the stored delegation of each object instance, which is looked up every time the object is called.
-// this is not the same as calling the original object constructor
 
-// further study on the extend object function for native JS, another way to extend objects by merging them with other objects.
-function extend(destination, source) {
-  for (var k in source) {
-    if (source.hasOwnProperty(k)) {
-      destination[k] = source[k];
-    }
-  }
-  return destination; 
-}
-// this allows for the object to be extended as shown in the tutorial.
-
-// with ES6 compliant JavaScript one can use the Object.Assign parameter to merge objects, as shown below
-// var car = function (loc){
-	// var obj  = {loc: loc};
-	// Object.assign(obj, Car.methods);
-	// return obj;
+var Cop = function (loc){
+	var obj = Car(log);
+	obj.call = function (){};
+	return obj;
 };
-// only withs with es6 compliant compilers, best to use a function callback instead
-
-
-// The following example has the same replationship to the Car.prototype
-// as our Car object function, so prototypes functions are essentially 
-// unique objects that can be applied to any variable.
-var Example = function (){
-	// returns the values of Car.prototype, and attaches that object to the
-	// Example variable
-	return Object.create(Car.prototype);
-}
---
+++ pre-subclasses/run.js
@@ -0,0 +1,31 @@
+// adding the new constructor to each of these instances of the Car object allows for the automatic addition of the Car.prototypes function
+// to each of instance of the object, and returns the objects value after the constructur function is ran
+var amy = new Car(1);
+amy.move();
+var ben = new Car(9);
+ban.move();
+var cal = new Car(2);
+cal.move();
+// the prototype.constructor property essentially mirrors the value of its parent object
+// for example refer to below:
+console.log(Car.prototype.constructor);
+// the above callback would duplicate the EXACT calues of the Car variable.
+
+// another example is:
+console.log(amy.constructor);
+// this calls the Car variable, since Car is the constructor of the Amy variable
+
+// the following looks for the .prototype object of the right argument in the left
+// arguments prototype chain.
+console.log(amy instanceof Car);
+
+// another example below
+var Dog = function (){
+	return {legs: 4, bark:alert};
+};
+
+var fido = Dog();
+
+log(fido instanceof Dog);
+// this would return a false value, since the Dog object does not have a Dog.prototype property
+// //
